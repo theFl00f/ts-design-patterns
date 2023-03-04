1168867f37a1fb758847343298970f86bf9ed374
Celeste Ellerby <celeste.ellerby@gmail.com>
2023-03-03 22:14:53 -0500

    Create Bridge class and page

---
+++ src/pages/BridgePage/Bridge.tsx
@@ -0,0 +1,52 @@
+// Refined Abstraction
+
+import { Logger } from '../../util';
+
+class DrawingAPI {
+  logger: Logger;
+  constructor() {
+    this.logger = new Logger();
+  }
+
+  drawCircle(x: Int, y: Int, radius: Float) {
+    throw new AbstractError();
+  }
+}
+
+export class CircleShape {
+  private x: Int;
+  private y: Int;
+  private radius: Float;
+  drawingApi: DrawingAPI;
+  constructor(x: Int, y: Int, radius: Float, drawingApi: DrawingAPI) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.drawingApi = drawingApi;
+  }
+
+  // low-level, i.e. Implementation specific
+  draw() {
+    this.drawingApi.drawCircle(this.x, this.y, this.radius);
+  }
+
+  // high-level, i.e. Abstraction specific
+  scale(percent: Float) {
+    this.radius *= percent;
+  }
+}
+
+// Concrete Implementors:
+// 1/2
+export class DrawingAPI1 extends DrawingAPI {
+  drawCircle(x: number, y: number, radius: number): void {
+    this.logger.log(`API1.circle at ${x}:${y}, radius ${radius}`);
+  }
+}
+
+// 2/2
+export class DrawingAPI2 extends DrawingAPI {
+  drawCircle(x: number, y: number, radius: number): void {
+    this.logger.log(`API2.circle at ${x}:${y}, radius ${radius}`);
+  }
+}
