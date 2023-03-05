c05a2a85cf79ec66e7e0ef991c3908e7bd810843
Celeste Ellerby <celeste.ellerby@gmail.com>
2023-03-05 16:47:11 -0500

    Add IteratorPage component

---
+++ src/pages/IteratorPage/IteratorPage.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react';
+import { Logger, LogRenderer } from '../../util';
+import { Aggregate, ConcreteIterator, IteratorActions } from './Iterator';
+
+const iteratorLog = <Type extends unknown>(
+  logger: Logger,
+  iterator: ConcreteIterator<Type>,
+  iteratorAction: IteratorActions<Type>
+) => {
+  switch (iteratorAction) {
+    case 'currentObject':
+      return logger.log(`Current object: ${iterator.currentObject()}`);
+    case 'firstObject':
+      return logger.log(`First object: ${iterator.firstObject()}`);
+    case 'nextObject':
+      return logger.log(`Next object: ${iterator.nextObject()}`);
+    case 'isDone':
+      return logger.log(iterator.isDone() ? 'Iterator is done!' : 'Not yet!');
+    default:
+      throw new Error('Unknown iteratorAction provided');
+  }
+};
+
+const IteratorPage: FC = () => {
+  const logger = new Logger();
+  const aggregate = new Aggregate<string>();
+  aggregate.items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
+  const iterator = new ConcreteIterator<string>(aggregate);
+
+  iteratorLog<string>(logger, iterator, 'nextObject');
+  iteratorLog<string>(logger, iterator, 'currentObject');
+  iteratorLog<string>(logger, iterator, 'nextObject');
+  iteratorLog<string>(logger, iterator, 'firstObject');
+  iteratorLog<string>(logger, iterator, 'nextObject');
+  iteratorLog<string>(logger, iterator, 'isDone');
+  iteratorLog<string>(logger, iterator, 'nextObject');
+
+  return <LogRenderer messages={logger.logs} />;
+};
+
+export default IteratorPage;
