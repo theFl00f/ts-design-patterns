c337010f625c1c95d5487e0a31f8aaae0a05e4fc
Celeste Ellerby <celeste.ellerby@gmail.com>
2023-03-04 21:48:49 -0500

    Add Decorator classes

---
+++ src/pages/DecoratorPage/Decorator.tsx
@@ -0,0 +1,66 @@
+import { Logger } from '../../util';
+
+class InterfaceError extends Error {
+  constructor() {
+    super('Interface error');
+  }
+}
+
+// Defined Interface
+export class Shape {
+  logger: Logger;
+  constructor() {
+    this.logger = new Logger();
+  }
+
+  draw() {
+    throw new InterfaceError();
+  }
+}
+
+// Concrete Implementors
+// 1/2
+export class Circle extends Shape {
+  draw() {
+    this.logger.log('Shape:Circle');
+  }
+}
+
+// 2/2
+export class Rectangle extends Shape {
+  draw() {
+    this.logger.log('Shape:Rectangle');
+  }
+}
+
+// Abstract class
+export class ShapeDecorator extends Shape {
+  decoratedShape: Shape;
+  constructor(decoratedShape: Shape) {
+    super();
+    this.decoratedShape = decoratedShape;
+    this.logger = decoratedShape.logger;
+  }
+
+  draw() {
+    this.decoratedShape.draw();
+  }
+}
+
+// Concrete implementor of Abstract class
+export class RedShapeDecorator extends ShapeDecorator {
+  redDecoratedShape: Shape;
+  constructor(decoratedShape: Shape) {
+    super(decoratedShape);
+    this.redDecoratedShape = decoratedShape;
+  }
+
+  setRedBorder(redDecoratedShape: Shape) {
+    this.logger.log('Border color: Red');
+  }
+
+  draw() {
+    this.redDecoratedShape.draw();
+    this.setRedBorder(this.redDecoratedShape);
+  }
+}
