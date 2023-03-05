409055b44636e7bf1939c079c96dbccc5703c006
Celeste Ellerby <celeste.ellerby@gmail.com>
2023-03-04 21:02:50 -0500

    Add CompositePage component

---
+++ src/pages/CompositePage/index.ts
@@ -0,0 +1,2 @@
+export { default as CompositePage } from './CompositePage'
+
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,4 +3,5 @@ export { AdapterPage } from './AdapterPage';
 export { BridgePage } from './BridgePage';
 export { BuilderPage } from './BuilderPage';
 export { CommandPage } from './CommandPage';
+export { CompositePage } from './CompositePage';
 export { HomePage } from './HomePage';
