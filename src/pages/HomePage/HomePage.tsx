89272cf9b355dae79298aa8ad37fe2c04a8f0464
Celeste Ellerby <celeste.ellerby@gmail.com>
2023-03-07 00:34:51 -0500

    Add nested links in homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,13 +1,23 @@
+import { groupBy, toPairs } from 'lodash';
 import { FC } from 'react';
 import { ROUTES } from '../../router';
 import HomePageLink from './HomePageLink';
 
 const HomePage: FC = () => {
+  const groupedRoutes = toPairs(groupBy(ROUTES, (route) => route.patternType));
+
   return (
     <ul>
-      {ROUTES.map((route) => (
-        <li key={`${route.id}-home-page-link`}>
-          <HomePageLink route={route} />
+      {groupedRoutes.map(([patternType, routes]) => (
+        <li key={patternType}>
+          {patternType}
+          <ul>
+            {routes.map((route, index) => (
+              <li key={`${route.id}-home-page-link`}>
+                <HomePageLink route={route} />
+              </li>
+            ))}
+          </ul>
         </li>
       ))}
     </ul>
