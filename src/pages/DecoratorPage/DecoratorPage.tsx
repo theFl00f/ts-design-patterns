ff7a32606e0f4a834f912ad334483a4e3b118971
Celeste Ellerby <celeste.ellerby@gmail.com>
2023-03-10 21:50:29 -0500

    Use ListLoggerRenderer in DecoratorPage

--- src/pages/DecoratorPage/DecoratorPage.tsx
+++ src/pages/DecoratorPage/DecoratorPage.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { LogRenderer } from '../../util';
+import ListLoggerRenderer from '../../util/ListLoggerRenderer';
 import { Circle, Rectangle, RedShapeDecorator } from './Decorator';
 
 const DecoratorPage: FC = () => {
@@ -7,18 +7,9 @@ const DecoratorPage: FC = () => {
   const redCircle = new RedShapeDecorator(new Circle());
   const redRectangle = new RedShapeDecorator(new Rectangle());
   const shapes = [circle, redCircle, redRectangle];
-  shapes.map((shape) => {
-    shape.draw();
-  });
-  return (
-    <ul>
-      {shapes.map((shape, index) => (
-        <li key={`shape-${index + 1}`} style={{ padding: '0.75em 0' }}>
-          <LogRenderer messages={shape.logger.logs} />
-        </li>
-      ))}
-    </ul>
-  );
+  shapes.map((shape) => shape.draw());
+  const loggers = shapes.map((shape) => shape.logger);
+  return <ListLoggerRenderer loggers={loggers} label={'shapes'} />;
 };
 
 export default DecoratorPage;
