5f8c04e6b6dc631327b1eb68ec5f81d5ac70695c
Celeste Ellerby <celeste.ellerby@gmail.com>
2023-03-10 22:02:18 -0500

    Use ListLoggerRenderer in FactoryMethodPage

--- src/pages/FactoryMethodPage/FactoryMethodPage.tsx
+++ src/pages/FactoryMethodPage/FactoryMethodPage.tsx
@@ -1,35 +1,26 @@
 import { FC } from 'react';
-import { LogRenderer } from '../../util';
+import { ListLoggerRenderer } from '../../util';
 import { ShapeFactory, ShapeType } from './FactoryMethod';
 
 const FactoryMethodPage: FC = () => {
   const shapeFactory = new ShapeFactory();
   // create a circle shape and call its draw method
   const circleShape = shapeFactory.getShape(ShapeType.Circle);
-  // call draw method of circle
   circleShape.draw();
 
   // create a rectangle shape and call its draw method
   const rectangleShape = shapeFactory.getShape(ShapeType.Rectangle);
-  // call draw method of rectangle
   rectangleShape.draw();
 
   // create a square shape and call its draw method
   const squareShape = shapeFactory.getShape(ShapeType.Square);
-  // call draw method of square
   squareShape.draw();
 
   const shapes = [circleShape, rectangleShape, squareShape];
 
-  return (
-    <ul>
-      {shapes.map((shape, index) => (
-        <li key={`shape=${index + 1}`}>
-          <LogRenderer messages={shape.logger.logs} />
-        </li>
-      ))}
-    </ul>
-  );
+  const loggers = shapes.map((shape) => shape.logger);
+
+  return <ListLoggerRenderer loggers={loggers} label={'shapes'} />;
 };
 
 export default FactoryMethodPage;
